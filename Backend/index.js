180cda69312acbe5d69ab78bfe3cf7059d71e7f2
unknown <tomygarnero9429@gmail.com>
2023-03-01 15:12:09 -0300

    Connectig DataBase

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,18 @@
-console.log("Desde Node.js");
+import express from "express";
+import dotenv from "dotenv";
+import conectarDB from "./config/db.js";
+
+const app = express();
+dotenv.config();
+
+conectarDB();
+
+app.use("/", (req,res) => {
+    res.send("Hola mundo");
+})
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor funcionando en el puerto ${PORT}`);
+})
