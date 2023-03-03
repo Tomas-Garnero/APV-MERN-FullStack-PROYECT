5e4de42b534592738f1f4696fa893d2d5b3a27ac
unknown <tomygarnero9429@gmail.com>
2023-03-03 12:17:43 -0300

    Getting Started with Account Confirmation

--- Backend/controllers/veterinarioController.js
+++ Backend/controllers/veterinarioController.js
@@ -26,4 +26,9 @@ const perfil = (req, res) => {
     res.json({ msg: "Mostrando perfil..."});
 };
 
-export { registrar, perfil };
+const confirmar = (req, res) => {
+    console.log(req.params.token);
+    res.json({msg: "Confirmando cuenta..."});
+};
+
+export { registrar, perfil, confirmar };
--- Backend/routes/veterinarioRoutes.js
+++ Backend/routes/veterinarioRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { registrar, perfil } from "../controllers/veterinarioController.js";
+import { registrar, perfil, confirmar } from "../controllers/veterinarioController.js";
 
 const router = express.Router();
 
 router.post("/", registrar);
-
 router.get("/perfil", perfil);
+router.get("/confirmar/:token", confirmar);
 
 export default router;
