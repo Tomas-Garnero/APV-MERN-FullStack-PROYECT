3304be19ec93f70fcc7879a81484608cd9c42695
unknown <tomygarnero9429@gmail.com>
2023-03-03 13:01:55 -0300

    Hashing User Passwords

--- Backend/models/Veterionario.js
+++ Backend/models/Veterionario.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 import generarId from "../helpers/generarId.js";
 
 const veterinarioSchema = mongoose.Schema({
@@ -35,6 +36,14 @@ const veterinarioSchema = mongoose.Schema({
         default: false
     }
 });
+
+veterinarioSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) {
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+})
 
 const Veterinario = mongoose.model("Veterinario", veterinarioSchema);
 export default Veterinario;
