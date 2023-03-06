b3faf513822fd61a86b7ad5d1ad3f2bdefefa000
unknown <tomygarnero9429@gmail.com>
2023-03-06 18:40:38 -0300

    Delete a Patient

--- Backend/controllers/pacienteController.js
+++ Backend/controllers/pacienteController.js
@@ -62,7 +62,23 @@ const actualizarPaciente = async (req, res) => {
 };
 
 const eliminarPaciente = async (req, res) => {
+    const { id } = req.params;
+    const paciente = await Paciente.findById(id);
 
+    if(!paciente) {
+        return res.status(404).json({msg: "No encontrado"});
+    }
+
+    if(paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
+        return res.json({msg: "Accion no valida"});
+    }
+
+    try {
+        await paciente.deleteOne();
+        res.json({msg: "Paciente eliminado"});
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 export { agregarPaciente, obtenerPacientes, obtenerPaciente, actualizarPaciente, eliminarPaciente };
