a637c1e4a6f217f1ae41e440474f3f0b05efd85f
unknown <tomygarnero9429@gmail.com>
2023-03-06 17:51:30 -0300

    Get a Specific Patient

--- Backend/controllers/pacienteController.js
+++ Backend/controllers/pacienteController.js
@@ -19,4 +19,27 @@ const obtenerPacientes = async (req, res) => {
     res.json(pacientes);
 };
 
-export { agregarPaciente, obtenerPacientes };
+const obtenerPaciente = async (req, res) => {
+    const { id } = req.params;
+    const paciente = await Paciente.findById(id);
+
+    console.log(paciente);
+
+    if(paciente.veterionario._id.toString() !== req.veterinario._id.toString()) {
+        return res.json({msg: "Accion no valida"});
+    }
+
+    if(paciente) {
+        res.json(paciente);
+    }
+};
+
+const actualizarPaciente = async (req, res) => {
+
+};
+
+const eliminarPaciente = async (req, res) => {
+
+};
+
+export { agregarPaciente, obtenerPacientes, obtenerPaciente, actualizarPaciente, eliminarPaciente };
