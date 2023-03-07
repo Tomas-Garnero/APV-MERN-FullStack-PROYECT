0c42df7a59354160ad0e7fb48a0090e362f59e58
unknown <tomygarnero9429@gmail.com>
2023-03-07 14:57:28 -0300

    First steps to send an Account Confirmation email

---
+++ Backend/helpers/emailRegistro.js
@@ -0,0 +1,14 @@
+import nodemailer from "nodemailer";
+
+const emailRegistro = () => {
+    const transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+}
+
+export default emailRegistro;
