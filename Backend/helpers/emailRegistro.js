6c35f83cfc3af87a19360cd2f739ef9acf830a3f
unknown <tomygarnero9429@gmail.com>
2023-03-07 15:36:39 -0300

    Sending the Email

--- Backend/helpers/emailRegistro.js
+++ Backend/helpers/emailRegistro.js
@@ -1,6 +1,6 @@
 import nodemailer from "nodemailer";
 
-const emailRegistro = () => {
+const emailRegistro = async (datos) => {
     const transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
@@ -9,6 +9,23 @@ const emailRegistro = () => {
             pass: process.env.EMAIL_PASS
         }
     });
+
+    const {email, nombre, token} = datos;
+
+    // Enviar el email
+    const info = await transporter.sendMail({
+        from: "APV - Administrador de Pacientes de Veterinaria",
+        to: email,
+        subject: "Confirma tu cuenta en APV",
+        text: "Confirma tu cuenta en APV",
+        html: `<p>Hola: ${nombre}, confirma tu cuenta en APV.</p>
+            <p>Tu cuenta ya esta lista, solo debes confirmarla en el siguiente enlace:
+            <a href="${process.env.FRONTEND_URL}/confirmar/${token}">Confirmar cuenta</a></p>
+
+            <p>Si tu no creaste esta cuenta, puedes ignorar este mensaje</p>
+        `
+    });
+    console.log("Mensaje enviado: %s", info.messageId);
 }
 
 export default emailRegistro;
