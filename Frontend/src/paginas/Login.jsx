bbd0e85f6aabfa815a3f39a4f761d85118000dec
unknown <tomygarnero9429@gmail.com>
2023-03-07 12:14:31 -0300

    Creating the User Registration page

--- Frontend/src/paginas/Login.jsx
+++ Frontend/src/paginas/Login.jsx
@@ -9,7 +9,7 @@ const Login = () => {
                     <span className="text-black"> Pacientes</span>
                 </h1>
             </div>
-            <div>
+            <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
                 <form>
                     <div className="my-5">
                         <label className="uppercase text-gray-600 block text-xl font-bold">
--- Frontend/src/paginas/Registrar.jsx
+++ Frontend/src/paginas/Registrar.jsx
@@ -1,9 +1,77 @@
-
+import { Link } from "react-router-dom";
 
 const Registrar = () => {
     return (
         <>
-            <h1>Desde Registrar</h1>
+            <div>
+                <h1 className="text-indigo-600 font-black text-6xl">
+                    Crea tu cuenta y Administra
+                    <span className="text-black"> tus Pacientes</span>
+                </h1>
+            </div>
+            <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
+                <form>
+                    <div className="my-5">
+                        <label className="uppercase text-gray-600 block text-xl font-bold">
+                            Nombre
+                        </label>
+                        <input 
+                            type="text" 
+                            placeholder="Tu Nombre" 
+                            className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" 
+                        />
+                    </div>
+                    <div className="my-5">
+                        <label className="uppercase text-gray-600 block text-xl font-bold">
+                            Email
+                        </label>
+                        <input 
+                            type="email" 
+                            placeholder="Email de Registro" 
+                            className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" 
+                        />
+                    </div>
+                    <div className="my-5">
+                        <label className="uppercase text-gray-600 block text-xl font-bold">
+                            Password
+                        </label>
+                        <input 
+                            type="password" 
+                            placeholder="Tu Password" 
+                            className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" 
+                        />
+                    </div>
+                    <div className="my-5">
+                        <label className="uppercase text-gray-600 block text-xl font-bold">
+                            Repetir Password
+                        </label>
+                        <input 
+                            type="password" 
+                            placeholder="Repite tu Password" 
+                            className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" 
+                        />
+                    </div>
+                    <input 
+                        type="submit"
+                        value="Iniciar Sesión"
+                        className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto"
+                    />
+                </form>
+                <nav className="mt-10 lg:flex lg:justify-between">
+                    <Link 
+                        to="/" 
+                        className="block text-center my-5 text-gray-500"
+                    >
+                        ¿Ya tienes una cuenta? Inicia Sesión
+                    </Link>
+                    <Link 
+                        to="/olvide-password"
+                        className="block text-center my-5 text-gray-500"
+                    >
+                        Olvide mi Password
+                    </Link>
+                </nav>
+            </div>
         </>
     )
 }
