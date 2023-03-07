422bf25ce964ad4783657e629b5a3abe69e8ff15
unknown <tomygarnero9429@gmail.com>
2023-03-07 12:44:06 -0300

    Validation of the Registration Form

--- Frontend/src/paginas/Registrar.jsx
+++ Frontend/src/paginas/Registrar.jsx
@@ -7,7 +7,25 @@ const Registrar = () => {
     const [ email, setEmail ] = useState("");
     const [ password, setPassword ] = useState("");
     const [ repetirPassword, setRepetirPassword ] = useState("");
-    
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if([nombre, email, password, repetirPassword].includes("")) {
+            console.log("Hay campos vacios");
+            return;
+        }
+
+        if(password !== repetirPassword) {
+            console.log("Los password no son iguales");
+            return;
+        }
+
+        if(password.length < 6) {
+            console.log("El password es muy corto. Minimo 6 caracteres");
+            return;
+        }
+    }
 
     return (
         <>
@@ -18,7 +36,7 @@ const Registrar = () => {
                 </h1>
             </div>
             <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="my-5">
                         <label className="uppercase text-gray-600 block text-xl font-bold">
                             Nombre
