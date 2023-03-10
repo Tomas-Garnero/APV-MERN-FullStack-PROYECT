6a6be3081282da68b17a443800b06d7edab9d1b1
unknown <tomygarnero9429@gmail.com>
2023-03-10 19:55:13 -0300

    Save User Password

--- Frontend/src/paginas/NuevoPassword.jsx
+++ Frontend/src/paginas/NuevoPassword.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import Alerta from "../components/Alerta";
 import clienteAxios from "../config/axios";
 
@@ -7,23 +7,44 @@ const NuevoPassword = () => {
 
     const [ password, setPassword] = useState("");
     const [ alerta, setAlerta ] = useState({});
-    const { tokenValido, setTokenValido } = useState(false);
+    const [ tokenValido, setTokenValido ] = useState(false);
+    const [ passwordModificado, setPasswordModificado] = useState(false);
 
     const params = useParams();
     const { token } = params;
+    
 
     useEffect(() => {
         const comprobarToken = async () => {
             try {
                 await clienteAxios(`/veterinarios/olvide-password/${token}`);
                 setAlerta({msg: "Coloca tu nuevo password"});
-                // setTokenValido(true);
+                setTokenValido(true);
             } catch (error) {
                 setAlerta({msg: "Hubo un error con el enlace", error: true})
             }
         }
         comprobarToken();
     }, [])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (password.length < 6) {
+            setAlerta({msg: "El password debe ser mínimo de 6 caracteres", error: true});
+            return;
+        }
+
+        try {
+            const url = `/veterinarios/olvide-password/${token}`;
+            const { data } = await clienteAxios.post(url, { password });
+            setAlerta({msg: data.msg});
+            setPasswordModificado(true);
+        } catch (error) {
+            setAlerta({msg: error.response.data.msg, error: true});
+        }
+
+    } 
 
     const { msg } = alerta;
 
@@ -37,8 +58,8 @@ const NuevoPassword = () => {
             </div>
             <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
                 {msg && <Alerta alerta={alerta} />}
-                {!tokenValido && (
-                    <form>
+                {tokenValido && (
+                    <form onSubmit={handleSubmit}>
                         <div className="my-5">
                             <label className="uppercase text-gray-600 block text-xl font-bold">
                                 Nuevo Password
@@ -57,6 +78,16 @@ const NuevoPassword = () => {
                             className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto"
                         />
                     </form>
+                )}
+                {passwordModificado && (
+                    <nav className="mt-10 lg:flex lg:justify-between">
+                        <Link 
+                            to="/" 
+                            className="block text-center my-5 text-gray-500"
+                        >
+                            Iniciar Sesión
+                        </Link>
+                </nav>
                 )}
             </div>
         </>
