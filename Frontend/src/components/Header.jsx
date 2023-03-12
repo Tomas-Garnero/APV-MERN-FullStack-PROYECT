0e179d5de07e7cbc5e0b5e3fdc6608dbfadb6d2d
unknown <tomygarnero9429@gmail.com>
2023-03-12 13:51:28 -0300

    Creating the Header of our Project

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,7 +1,21 @@
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
-        <h1>Header</h1>
+        <header className="py-10 bg-indigo-600">
+            <div className="container mx-auto flex justify-between items-center">
+                <h1 className="font-bold text-2xl text-indigo-200">
+                    Administrador de Pacientes de <span className="text-white font-black"> Veterinaria</span>
+                </h1>
+                <nav className="flex gap-4">
+                    <Link to="/admin" className="text-white text-sm uppercase font-bold">Pacientes</Link>
+                    <Link to="/admin" className="text-white text-sm uppercase font-bold">Perfil</Link>
+                    <button type="button" className="text-white text-sm uppercase font-bold">
+                        Cerrar Sesión
+                    </button>
+                </nav>
+            </div>
+        </header>
     )
 }
 
